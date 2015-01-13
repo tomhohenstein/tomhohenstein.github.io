ba84b254411bf942ca3288afcabe8b9622713ed5
Tom Hohenstein <sirtah@gmail.com>
2015-01-13 10:39:20 -0500

    update bar chart 2

--- _site/assets/js/p/barchart.2.js
+++ _site/assets/js/p/barchart.2.js
@@ -1,4 +1,45 @@
+var obj = []
+
 $( document ).ready(function (){
 
+//Show values on the x-axis, and categories on the y-axis. 
+//Make x-axis a linear scale and y-axis an ordinal scale. 
 
-}
+	
+
+	//load data from json file 
+	d3.json("/projects/barchart-2/data.json", function(error, data){
+		// send any errors to the console
+		if(error) return console.warn(error);
+
+		//for outside access 
+		obj = data; 
+
+		console.log(data); 
+
+		//set widths, heights, and margins
+		var width = $("article").width(),
+		height = 500,
+		margin = {top: 40, right: 40, bottom: 40, left:40}
+		inWidth = width - margin.right - margin.left, //inner width
+		inHeight = height - margin.top - margin.bottom; //inner height
+
+		// set x axis scale 
+		// should be linear scale 
+		var x = d3.scale.linear()
+					.domain(0, d3.max(data, function(d){ return d.Quantity }))
+					.range([0, inWidth]);
+
+		console.log(x);
+
+		/*
+		//set y axis scale
+		//should be ordinal scale 
+		var y = d3.scale.ordinal()
+					.domain(data.map(function(d){ return d.fruits }))
+					.range([0, inHeight]); 			
+
+		console.log(y);
+*/
+	});//end d3.json
+}); // .ready() 
--- projects/barchart-2/index.md
+++ projects/barchart-2/index.md
@@ -1,24 +1,11 @@
 ---
 layout: post
 title: Bar Chart 2
-css: p/barchart.css
-tags: project, javascript, D3
+css:
+tags: library juice, javascript, D3
 javascript:
 - l/d3.min.js
 - p/barchart.2.js
 ---
-<!-- <a class="btn btn-default btn-lg" href="/my-first-d3-bar-chart/" role="button">Read the Post</a> --> 
-<div class="dropdown">
-  <button class="btn btn-default btn-lg dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-    Sort Options
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-    <li role="presentation"><a data-val="1" role="menuitem"  tabindex="-1" href="#">A-Z</a></li>
-    <li role="presentation"><a data-val="2" role="menuitem" tabindex="-1" href="#">Z-A</a></li>
-    <li role="presentation"><a data-val="3" role="menuitem" tabindex="-1" href="#">Highest to Lowest</a></li>
-    <li role="presentation"><a data-val="4" role="menuitem" tabindex="-1" href="#">Lowest to Highest</a></li>
-  </ul>
-</div>
-<h3 id="sorted">Sorted: A-Z</h3>
+
 <svg id="chart"></svg>
