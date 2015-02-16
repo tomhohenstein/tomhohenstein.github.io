a3fa1dcf7b59b828a22c87e5480b17510be4cd9a
Tom Hohenstein <thomas.a.hohenstein@gmail.com>
2015-02-16 18:17:03 -0500

    sync

--- _site/assets/js/p/cross.demo.js
+++ _site/assets/js/p/cross.demo.js
@@ -2,6 +2,7 @@ d3.csv("dc.data.csv", function(csv){
 	var data = crossfilter(csv);  
 	//set dimension vars
 	var question_half_hour = data.dimension(function (d){
+			//console.log(d.question_half_hour)
 			return d.question_half_hour; 
 		}),
 		question_weekday = data.dimension(function (d){
@@ -10,33 +11,37 @@ d3.csv("dc.data.csv", function(csv){
 		location_name = data.dimension(function (d){
 			return d.location_name; 
 		});
-	
+
 	//set group vars 
 	question_half_hour_group = question_half_hour.group().reduceCount(); 
 	question_weekday_group = question_weekday.group().reduceCount(); 
 	location_name_group = location_name.group().reduceCount(); 
 
-	var question_half_hour_chart = dc.rowChart("#question_half_hour")
-		.width(700) 
-		.height(900)
-		.margins({top:10, left:10, right:10, bottom:30})
+	var question_half_hour_chart = dc.barChart("#question_half_hour")
+		.width(1000) 
+		.height(300)
+		.margins({top:10, left:40, right:40, bottom:30})
 		.dimension(question_half_hour)
 		.group(question_half_hour_group)
 		.label(function(d){ return d.key; })
 		.colors(d3.scale.category20c())
-		.elasticX(true)
-		.xAxis().ticks(4); 
+		.elasticY(true)
+		.x(d3.scale.ordinal().domain(question_half_hour_group))
+        .xUnits(dc.units.ordinal)
+        .renderHorizontalGridLines(true)
+        .yAxis().ticks(4);
 
 	var question_weekday = dc.rowChart("#question_weekday")
-		.width(300) 
-		.height(500)
+		.width(250) 
+		.height(250)
 		.margins({top:10, left:10, right:10, bottom:30})
 		.dimension(question_weekday)
 		.group(question_weekday_group)
 		.label(function(d){ return d.key; })
-		.colors(["#edf8fb", "#ccece6", "#99d8c9", "#66c2a4", "#41ae76", "#238b45", "#005824"])
+		.colors(["rgb(65, 174, 118)"])
 		.elasticX(true)
-		.xAxis().ticks(4); 
+		.xAxis().ticks(4);
+
 	
 	var location_name_chart = dc.pieChart("#location_name")
 		.dimension(location_name)
