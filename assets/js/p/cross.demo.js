47b368d266d1041b5a71baf1898ebdf99c8709cb
Tom Hohenstein <sirtah@gmail.com>
2015-02-17 11:06:17 -0500

    j err

--- assets/js/p/cross.demo.js
+++ assets/js/p/cross.demo.js
@@ -8,7 +8,8 @@ d3.csv("dc.data.csv", function(csv){
 		question_weekday = data.dimension(function (d){
 			var day = d.question_weekday;
 			var name = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-			//return d.question_weekday; 
+			//return d.question_weekday;
+			console.log(name);
 			console.log(day+"."+name[day])
 			return day+"."+name[day]; 
 		}),
