face4f144512e4522544fc1b37cc4b4e1a3c377f
Tom Hohenstein <sirtah@gmail.com>
2015-02-17 09:32:12 -0500

    j err

--- assets/js/p/cross.demo.js
+++ assets/js/p/cross.demo.js
@@ -57,7 +57,7 @@ d3.csv("dc.data.csv", function(csv){
 		.radius([100])
 		.innerRadius([50])
 		.colors(["#fdae61", "#ffffbf", "#abdda4", "#2b83ba", "#d7191c"])
-		.legend(dc.legend().x(400).y(10).itemHeight(13).gap(5));
+		.legend(dc.legend().x(10).y(10));
  
 	var patron_type_chart = dc.rowChart("#patron_type")
 		.width(250) 
