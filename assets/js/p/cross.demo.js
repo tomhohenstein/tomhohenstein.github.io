d1e42f72941de1188d3690ce148503313ee2fe74
Tom Hohenstein <sirtah@gmail.com>
2015-02-17 09:50:06 -0500

    j err

--- assets/js/p/cross.demo.js
+++ assets/js/p/cross.demo.js
@@ -56,7 +56,6 @@ d3.csv("dc.data.csv", function(csv){
 		.group(location_name_group)
 		.radius([100])
 		.innerRadius([50])
-		.renderLable(false)
 		.colors(["#fdae61", "#ffffbf", "#abdda4", "#2b83ba", "#d7191c"])
 		.legend(dc.legend().x(10).y(10));
  
