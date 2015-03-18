88c18d65cc29ab3af5efbb8b9aa22ee644209f27
Tom Hohenstein <thomas.a.hohenstein@gmail.com>
2015-03-18 09:42:59 -0400

    publish map of the usa post

--- assets/js/p/usa.map.js
+++ assets/js/p/usa.map.js
@@ -8,40 +8,36 @@ $( document ).ready(function (){
 						.translate([width / 2, height / 2]);
 
 	var path = d3.geo.path()
-    			.projection(projection)
+    			.projection(projection);
 
 	var map = d3.select("#map").append("svg")
     				.attr("width", width)
     				.attr("height", height);				
 
 	queue()
-      	.defer(d3.json, "/assets/data/usa.json")
+      .defer(d3.json, "/assets/data/usa.json")
     	.await(ready);
 
 	function ready(error, us) {
 
 	   console.log(us);
 
-     //us data
-     map.append("path")
-          .datum(topojson.feature(us, us.objects.state))
-          .attr("class", "land")
-          .attr("d", path)
-    
+     //us data    
       map.append("g")
     		.attr("class", "state")
     		.selectAll("path")
     		.data(topojson.feature(us, us.objects.state).features)
     		.enter()
     		.append("path") 
-    		.attr("d", path)
-    	
+    		.attr("d", path);
+ 	
     	map.append("g")
-    		.attr("class", "county")
+    		.attr("class", "counties")
     		.selectAll("path")
     		.data(topojson.feature(us, us.objects.county).features)
     		.enter()
     		.append("path") 
-    		.attr("d", path) 
+    		.attr("d", path)
+        .attr("class", "county");
   	}
 }); //eof 
